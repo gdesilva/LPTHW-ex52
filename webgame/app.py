dd2f01fed65aa42f0db142740d403c928c3eab5e
Gus de Silva <gusdsw@outlook.com>
2017-08-10 00:23:20 +0100

    Add files via upload

---
+++ webgame/app.py
@@ -0,0 +1,51 @@
+import web
+import map
+
+
+urls = (
+    '/game', 'GameEngine',
+    '/', 'Index',
+)
+
+app = web.application(urls, globals())
+
+# Zed: little hack so that debug mode works with sessions
+if web.config.get('_session') is None:
+    store = web.session.DiskStore('sessions')
+    session = web.session.Session(app, store,
+                                  initializer={'room': None})
+    web.config._session = session
+else:
+    session = web.config._session
+
+
+render = web.template.render('templates/', base="layout")
+
+class Index(object):
+    def GET(self):
+        # Zed: this is used to "setup" the session with starting values
+        session.room = map.START
+        web.seeother("/game")
+
+class GameEngine(object):
+
+    def GET(self):
+        if session.room:
+            return render.show_room(room=session.room)
+        else:
+            # Zed: why is this here? do you need it?
+            return render.you_died()
+
+
+    def POST(self):
+        form = web.input(action=None)
+
+        # Zed: there is a bug here, can you fix it?
+        if session.room and form.action:
+            session.room = session.room.go(form.action)
+
+        web.seeother("/game")
+
+
+if __name__ == "__main__":
+    app.run()
